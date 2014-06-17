3996af432dab95d58283a0dbeac1e4926e2b0924
Andy Clouser <aclouser117@gmail.com>
2014-06-16 20:46:22 -0400

    added introduction text

--- main.cc
+++ main.cc
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "Testing" << endl;
+	string intro;
+	int width,height;
+	width = 20;
+	height = 20;
+	intro = "Welcome to the Game of Life!\nThis simulation will run with a width of "
+			+to_string(width)+ " cells, and a height of " +to_string(height)+ " cells.";
+	cout << intro << endl;
 }
