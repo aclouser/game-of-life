752520e36f6e16f2a225f1203639e921d47bae93
Andy Clouser <aclouser117@gmail.com>
2014-06-25 19:16:32 -0400

    Adding board.cc

---
+++ board.cc
@@ -0,0 +1,24 @@
+#include "board.h"
+
+using namespace std;
+
+Board::Board(int width, int height)
+{
+	this->width = width;
+	this->height = height;
+}
+
+void Board::createBoard_Random()
+{
+	cout << "\n POOP " < endl;	
+}
+
+void Board::printBoard()
+{
+
+}
+
+void Board::updateBoard()
+{
+
+}
